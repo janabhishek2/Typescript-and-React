e19cb2ea404edd9f15890a82846c4e45e284cc68
Abhishek Jan <abhishek.jan@brevo.com>
2025-10-11 12:59:54 +0530

    object-type definition

---
+++ a.ts
@@ -0,0 +1,13 @@
+let user: {
+    name: {
+        firstName: string,
+        lastName: string
+    };
+    age: number;
+} = {
+    name: {
+        firstName: "Abhishek",
+        lastName: "Jan"
+    },
+    age: 24
+}
