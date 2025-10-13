621cb99832f6ffa62cc424cead5bca5743daf5aa
Abhishek Jan <abhishek.jan@brevo.com>
2025-10-13 11:23:46 +0530

    inf errred null and type narrowing

--- a.ts
+++ a.ts
@@ -1,24 +1,8 @@
-function log(msg: string): void {
-    console.log(msg);
+const inputEl = document.getElementById("input-element");
+
+// type narrowing
+if(!inputEl) {
+    throw new Error("input element not found !!");
 }
 
-function performJob(cb: (m: string) => void) {
-    cb("Job executed !!");
-};
-
-type Greet = (msg: string) => void
-
-type User = {
-    name: string;
-    age: number;
-    greet: Greet,
-    randomFunc: Function
-}
-
-const user: User = {
-    name: "Abhishek",
-    age: 24,
-    greet: (a) => {},
-    randomFunc: () => {}
-};
-
+inputEl.value = 1234;
