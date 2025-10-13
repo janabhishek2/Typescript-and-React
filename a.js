d3b6e1369031d1a0c36c729b8ea966b74fb62ddd
Abhishek Jan <abhishek.jan@brevo.com>
2025-10-13 11:23:46 +0530

    inf errred null and type narrowing

--- a.js
+++ a.js
@@ -1,13 +1,3 @@
-function log(msg) {
-    console.log(msg);
-}
-function performJob(cb) {
-    cb("Job executed !!");
-}
-;
-var user = {
-    name: "Abhishek",
-    age: 24,
-    greet: function (a) { },
-    randomFunc: function () { }
-};
+var a;
+a = undefined;
+a = "123";
