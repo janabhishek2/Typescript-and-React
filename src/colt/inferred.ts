86110d781e422096efcd1915e2eda712119614dd
Abhishek Jan <abhishek.jan@brevo.com>
2026-04-11 12:51:26 +0530

    chore: alertUser

--- src/colt/inferred.ts
+++ src/colt/inferred.ts
@@ -4,4 +4,14 @@ function multiply(x: number, y: number): number {
 
 const ans = multiply(3, 4);
 
-console.log(ans);
+const addNums = (x: number, y: number): number => {
+    return x + y;
+};
+
+const ans1 = addNums(3, 4);
+
+function alertUser(msg: string): void {
+    console.log(msg);
+    console.log(msg);
+}
+
