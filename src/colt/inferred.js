6a3192ca561fe57e7cb1174a3f5073dca6fb5765
Abhishek Jan <abhishek.jan@brevo.com>
2026-04-11 12:51:26 +0530

    chore: alertUser

--- src/colt/inferred.js
+++ src/colt/inferred.js
@@ -2,4 +2,8 @@ function multiply(x, y) {
     return x * y;
 }
 var ans = multiply(3, 4);
-console.log(ans);
+var addNums = function (x, y) {
+    return x + y;
+};
+var ans1 = addNums(3, 4);
+console.log(ans1);
