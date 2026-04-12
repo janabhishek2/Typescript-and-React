80c58ab0f5ed4974f6ed7a10f881d51604e003e4
Abhishek Jan <abhishek.jan@brevo.com>
2026-04-13 00:14:59 +0530

    feat: arrays

---
+++ src/colt/arrays.js
@@ -0,0 +1,13 @@
+var nums = [1, 2, 3, 4];
+console.log(nums);
+var names = ["Abhishek", "Joan"];
+var point1 = {
+    x: 1,
+    y: 2
+};
+var point2 = {
+    x: 3,
+    y: 5
+};
+var points = [point1, point2];
+console.log(points);
---
+++ src/colt/arrays.ts
@@ -0,0 +1,24 @@
+const nums: number[] = [1, 2,3 ,4];
+
+console.log(nums);
+
+const names: Array<string> = ["Abhishek", "Joan"];
+
+type Point = {
+    x: number,
+    y: number
+};
+
+const point1: Point = {
+    x: 1,
+    y: 2
+};
+
+const point2: Point = {
+    x: 3,
+    y: 5
+}
+
+const points: Point[] = [point1, point2];
+
+console.log(points);
