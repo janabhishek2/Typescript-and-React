f1f50097cea063d13c9a4080b84b1338241febcb
Abhishek Jan <abhishek.jan@brevo.com>
2026-04-13 08:50:18 +0530

    feat: exercies

---
+++ src/colt/exercicesArray.ts
@@ -0,0 +1,51 @@
+// **********************************************
+// ******************* PART 1 *******************
+// **********************************************
+// Create an empty array of numbers called "ages":
+
+
+const ages: number[] = [];
+
+// **********************************************
+// ******************* PART 2 *******************
+// **********************************************
+// Create an array variable called gameBoard that starts as an empty array.
+// It should be typed to hold a 2 dimensional array of strings
+
+const gameBoard: string[][] = [];
+
+// **********************************************
+// ******************* PART 3 *******************
+// **********************************************
+// Create a Product type that contains a name and a price.
+// An example product could be:
+// {name: "coffee mug", price: 11.50}
+
+type Product = {
+    name: string,
+    price: number
+}
+
+// **********************************************
+// ******************* PART 4 *******************
+// **********************************************
+// Write a function called getTotal that accepts an array of Product types
+// It should return the sum of all the products' prices
+
+const getTotal = (products: Product[]): number => {
+    return products.reduce((acc, curr) => {
+        return acc + curr?.price;
+    }, 0);
+}
+
+const products: Product[] = [{
+    name: "Dahi",
+    price: 200
+}, {
+    name: "Soya",
+    price: 300
+}];
+
+const ans = getTotal(products);
+
+console.log(ans);
