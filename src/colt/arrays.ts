3351f6ffb29d1f3e4fa6007e334b30fb30f5e214
Abhishek Jan <abhishek.jan@brevo.com>
2026-04-13 08:38:24 +0530

    feat: board

--- src/colt/arrays.ts
+++ src/colt/arrays.ts
@@ -1,24 +1,34 @@
-const nums: number[] = [1, 2,3 ,4];
+const nums: number[] = [1, 2, 3, 4];
 
 console.log(nums);
 
 const names: Array<string> = ["Abhishek", "Joan"];
 
 type Point = {
-    x: number,
-    y: number
+  x: number;
+  y: number;
 };
 
 const point1: Point = {
-    x: 1,
-    y: 2
+  x: 1,
+  y: 2,
 };
 
 const point2: Point = {
-    x: 3,
-    y: 5
-}
+  x: 3,
+  y: 5,
+};
 
 const points: Point[] = [point1, point2];
 
-console.log(points);
+// multi dimension arrays
+
+// array of array of strings
+type Board = number[][];
+
+const board: Board = [
+  [1, 2, 3],
+  [4, 5, 6],
+];
+
+console.log(board);
